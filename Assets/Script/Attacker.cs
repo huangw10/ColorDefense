a2a22551aba310ab7e42ca9253ac605540612b9c
huangw10 <huangw10@uci.edu>
2019-05-28 10:41:54 -0700

    12312
    123123

---
+++ Assets/Script/Attacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Attacker : NetworkBehaviour
+{
+    public GameObject related_Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        related_Player = GameObject.FindWithTag("xyz");
+        this.transform.position = new Vector3(this.transform.position.x, related_Player.transform.position.y, 0); 
+    }
+}
---
+++ Assets/Script/Competitive_player_connection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Competitive_player_connection : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject playerobject;
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        CmdSpawnplayer();
+    }
+    [Command]
+    public void CmdSpawnplayer()
+    {
+        GameObject player = Instantiate(playerobject);//??????
+        NetworkServer.Spawn(player);
+        player.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
