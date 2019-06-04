a2575661e36d44e039e8c39699c617feceb930ba
huangw10 <huangw10@uci.edu>
2019-06-04 14:25:03 -0700

    2131
    sadad

--- Assets/Script/Competitive_gamemanager.cs
+++ Assets/Script/Competitive_gamemanager.cs
@@ -8,10 +8,18 @@ public class Competitive_gamemanager : MonoBehaviour
     public UnityEvent leftwin;
     public UnityEvent rightwin;
     static public Competitive_gamemanager instance;
+    public GameObject penal;
     // Start is called before the first frame update
     private void Awake()
     {
         instance = this;
+        leftwin.AddListener(endofgame);
+        rightwin.AddListener(endofgame);
+    }
+
+    void endofgame()
+    {
+        penal.SetActive(true);
     }
     void Start()
     {
